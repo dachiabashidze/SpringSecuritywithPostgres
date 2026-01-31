3449f40ed4954e0a3dd25f19881c9aa9214572ca
dachiabashidze <dachi.abashidze@icloud.com>
2026-01-31 15:47:22 +0400

    added authentication

--- src/main/java/com/DB/SecurityProject/config/SecurityConfig.java
+++ src/main/java/com/DB/SecurityProject/config/SecurityConfig.java
@@ -4,12 +4,15 @@ import com.DB.SecurityProject.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password4j.BcryptPassword4jPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -45,5 +48,11 @@ public class SecurityConfig {
 
 
         return http.build();
+    }
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
