de472ee73257ed2ee09703e29e9aedcf27af578d
dachiabashidze <dachi.abashidze@icloud.com>
2026-02-04 20:43:42 +0400

    can't add global exception handler, reason:static html

--- src/main/java/com/DB/SecurityProject/dto/RegisterRequest.java
+++ src/main/java/com/DB/SecurityProject/dto/RegisterRequest.java
@@ -9,15 +9,15 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class RegisterRequest {
-    @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+//    @NotBlank(message = "Username is required")
+//    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
 
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid email format")
+//    @NotBlank(message = "Email is required")
+//    @Email(message = "Invalid email format")
     private String email;
 
-    @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters")
+//    @NotBlank(message = "Password is required")
+//    @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
 }
